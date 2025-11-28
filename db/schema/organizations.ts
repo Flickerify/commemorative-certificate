d35361ff861f72079c0c80e223b616f6182123b2
Sebastien <15332099+Morpheusse@users.noreply.github.com>
2025-11-28 15:54:28 +0100

    Add organizations schema to database with necessary fields

--- db/schema/organizations.ts
+++ db/schema/organizations.ts
@@ -1,1 +1,9 @@
+import { int, mysqlTable, serial, varchar, json, boolean, timestamp } from 'drizzle-orm/mysql-core';
 
+export const organizations = mysqlTable('organizations', {
+  id: serial().primaryKey(),
+  workosId: varchar({ length: 255 }).notNull().unique(),
+  convexId: varchar({ length: 255 }).notNull().unique(),
+  updatedAt: timestamp().notNull(),
+  createdAt: timestamp().notNull().defaultNow(),
+});
